2c43cd364fe864d8435b9c02ae801f3c4138f199
William Mallard <wjm@llard.net>
2026-08-05 23:12:30 -0700

    Silence TrackMate's internal logging for headless operation.
    The parameters it reported are already written to the output XML by
    appendSettings(), so the log duplicated them in a form nothing reads.

--- 3_Fiji_TrackMate.py
+++ 3_Fiji_TrackMate.py
@@ -9,8 +9,6 @@
 #   --console \
 #   --run 3_Fiji_TrackMate.py 'basedir="/path/to/data"'
 
-from ij import IJ
-
 import java.io.File as File
 
 from loci.plugins import BF
@@ -21,7 +19,6 @@ from fiji.plugin.trackmate import Settings
 from fiji.plugin.trackmate import TrackMate
 from fiji.plugin.trackmate import Logger
 
-from fiji.plugin.trackmate.util import TMUtils
 from fiji.plugin.trackmate.io import TmXmlWriter
 
 from fiji.plugin.trackmate.detection import LogDetectorFactory
@@ -120,61 +117,6 @@ def open_bioformats(filename, channel=0, frame=-1, series=0):
 
     return imps[0]
 
-def log_startup(logger, settings):
-    '''
-    Adapted from:
-    TrackMate/src/main/java/fiji/plugin/trackmate/gui/wizard/descriptors/StartDialogDescriptor.java
-    '''
-    # aboutToDisplayPanel():
-    PUB1_URL = "https://doi.org/10.1101/2021.09.03.458852"
-
-    PUB1_TXT = ("Ershov D, Phan M-S, Pylvanainen JW, Rigaud SU, et al. "
-                "'Bringing TrackMate in the era of machine-learning and deep-learning.'"
-                "bioRxiv. 2021; doi:10.1101/2021.09.03.458852")
-
-    welcomeMessage = (TrackMate.PLUGIN_NAME_STR + " v" + TrackMate.PLUGIN_NAME_VERSION
-                      + " started on:\n" + TMUtils.getCurrentTimeString())
-
-    logger.log(welcomeMessage)
-    logger.log("Please note that TrackMate is available through Fiji, and is based on a publication. "
-               "If you use it successfully for your research please be so kind to cite our work:")
-    logger.log(PUB1_TXT)
-    logger.log(PUB1_URL)
-    logger.log("and / or:")
-    logger.log("Tinevez, JY.; Perry, N. & Schindelin, J. et al. (2017), "
-               "'TrackMate: An open and extensible platform for single-particle tracking.', "
-               "Methods 115: 80-90, PMID 27713081.")
-    logger.log("https://www.sciencedirect.com/science/article/pii/S1046202316303346")
-
-    logger.log("\nNumerical feature analyzers:")
-    logger.log(settings.toStringFeatureAnalyzersInfo())
-
-    # aboutToHidePanel():
-    logger.log("Image region of interest:")
-    logger.log(settings.toStringImageInfo())
-
-def log_detector(logger, settings):
-    '''
-    Adapted from:
-    TrackMate/src/main/java/fiji/plugin/trackmate/gui/wizard/descriptors/SpotDetectorDescriptor.java
-    '''
-    # aboutToHidePanel():
-    logger.log("Configured detector "
-               + settings.detectorFactory.getName()
-               + " with settings:")
-    logger.log(TMUtils.echoMap(settings.detectorSettings, 2))
-
-def log_tracker(logger, settings):
-    '''
-    Adapted from:
-    TrackMate/src/main/java/fiji/plugin/trackmate/gui/wizard/descriptors/SpotTrackerDescriptor.java
-    '''
-    # aboutToHidePanel():
-    logger.log("Configured tracker "
-               + settings.trackerFactory.getName()
-               + " with settings:")
-    logger.log(TMUtils.echoMap(settings.trackerSettings, 2))
-
 def run_TrackMate(filename):
     '''
     Run TrackMate on a single stack.
@@ -185,22 +127,18 @@ def run_TrackMate(filename):
     im = open_bioformats(filename)
 
     model = Model()
-    model.setLogger(Logger.IJ_LOGGER)
+    model.setLogger(Logger.VOID_LOGGER)
 
     logger = model.getLogger()
-    logger.log('\Clear')
 
     settings = Settings(im)
     settings.addAllAnalyzers()
-
-    log_startup(logger, settings)
 
     #
     # Configure detector.
     #
     settings.detectorFactory = LogDetectorFactory()
     settings.detectorSettings = DetectorSettings
-    log_detector(logger, settings)
 
     #
     # Configure spot filters.
@@ -216,7 +154,6 @@ def run_TrackMate(filename):
     #
     settings.trackerFactory = KalmanTrackerFactory()
     settings.trackerSettings = TrackerSettings
-    log_tracker(logger, settings)
 
     #
     # Configure track filters.
@@ -242,7 +179,6 @@ def run_TrackMate(filename):
     outFile = File(xml_path, xml_filename)
 
     writer = TmXmlWriter(outFile, logger)
-    writer.appendLog(IJ.getLog())
     writer.appendModel(trackmate.getModel())
     writer.appendSettings(trackmate.getSettings())
     writer.writeToFile()
